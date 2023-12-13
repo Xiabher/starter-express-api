36f77d94f7aaff84f812a6c2e236f79f038a30e8
Xiabher <99346955+Xiabher@users.noreply.github.com>
2023-12-13 23:42:50 +0100

    added frontend form and backend route greet

--- index.js
+++ index.js
@@ -4,4 +4,13 @@ app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
+
+// New Route for Greeting
+app.post('/greet', express.json(), (req, res) => {
+    const { name } = req.body;
+    const greeting = `Hello, ${name}!`;
+    console.log(greeting);
+    res.json({ message: greeting });
+});
+
 app.listen(process.env.PORT || 3000)
